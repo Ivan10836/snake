702ab4ee4ca09c9f054dd1d8e4902e9fb3b533de
Ivan10836 <164682388+Ivan10836@users.noreply.github.com>
2026-05-21 22:35:34 +0200

    input simple synk

--- Input.ts
+++ Input.ts
@@ -11,11 +11,23 @@ class Input {
         if (this.dir == 4) {
             this.dir = 0
         }
+        if (isMaster) {
+            radio.sendNumber(this.dir)
+        }
     }
     ButtB(){
         this.dir--
         if(this.dir == -1){
             this.dir = 3
+        }
+        if(isMaster){
+            radio.sendNumber(this.dir)
+        }
+    }
+
+    TakeInputPacket(dir: number){
+        if(!isMaster){
+            this.dir = dir
         }
     }
 }
--- main.ts
+++ main.ts
@@ -3,26 +3,41 @@ function HideVectorArr (v: any[]) {
         HideVector(v[j])
     }
 }
-radio.onReceivedNumber(function (receivedNumber) {
-    if (apple.NewApplePosTranslateStage != 0 && isConect && !(isMaster)) {
-        apple.TakeNewApplePosPackage(receivedNumber)
-    }
-})
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    if (!(isConect)) {
-        radio.sendString("IMaster")
-    } else {
-        _input.ButtA()
-    }
-})
-function SchowVectorArr (v: any[]) {
+function SchowVectorArr(v: any[]) {
     for (let i = 0; i <= v.length - 1; i++) {
         SchowVector(v[i])
     }
 }
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    _input.ButtB()
+function SchowVector(v: Vector2Int) {
+    if (isMaster) {
+        led.plot(v.x, v.y)
+    } else {
+        led.plot(v.x - 5, v.y)
+    }
+}
+function HideVector(v: Vector2Int) {
+    if (isMaster) {
+        led.unplot(v.x, v.y)
+    } else {
+        led.unplot(v.x - 5, v.y)
+    }
+}
+
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    if (!(isConect)) {
+        radio.sendString("IMaster")
+    } else {
+        if(isMaster){
+            _input.ButtA()
+        }
+    }
 })
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    if (isMaster) {
+        _input.ButtB()
+    }
+})
+
 radio.onReceivedString(function (receivedString) {
     // Conect
     if (receivedString == "IMaster" && !(isConect)) {
@@ -57,31 +72,31 @@ radio.onReceivedString(function (receivedString) {
         basic.clearScreen()
     }
 })
+radio.onReceivedNumber(function (receivedNumber) {
+    if (apple.NewApplePosTranslateStage != 0 && isConect && !(isMaster)) {
+        apple.TakeNewApplePosPackage(receivedNumber)
+    }else if(!isMaster && isConect){
+        _input.TakeInputPacket(receivedNumber)
+    }
+})
+
 let isConect = false
 let isMaster = false
-function SchowVector(v: Vector2Int) {
-    if (isMaster) {
-        led.plot(v.x, v.y)
-    } else {
-        led.plot(v.x - 5, v.y)
-    }
-}
-function HideVector(v: Vector2Int) {
-    if (isMaster) {
-        led.unplot(v.x, v.y)
-    } else {
-        led.unplot(v.x - 5, v.y)
-    }
-}
+
 radio.setGroup(55)
+
 while (!(isConect)) {
     basic.pause(1)
 }
+
 basic.pause(2000)
+
 let snake = new Snake()
 let apple = new Apple()
 let _input = new Input()
+
 basic.pause(1000)
+
 while (true) {
     snake.Tackt(apple, _input);
 basic.pause(1000)
